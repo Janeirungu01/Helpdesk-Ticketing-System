04ff433683a3a28c4b79dd4d604c9bfc3675870e
Janeirungu01 <jwirungu96@gmail.com>
2025-03-31 11:50:23 +0300

    add select options for category and branch

--- src/components/User/TicketForm.jsx
+++ src/components/User/TicketForm.jsx
@@ -2,21 +2,20 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 
-export default function TicketForm({setTickets}) {
+export default function TicketForm({ setTickets }) {
   const navigate = useNavigate();
-
 
   const initialFormState = {
     subject: "",
     category: "",
-    department: "",
-    email: "",
+    branch: "",
+    date: "",
     description: "",
     priority: "Low",
   };
 
   const [formData, setFormData] = useState(initialFormState);
-
+  const today = new Date().toISOString().split("T")[0]; 
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -25,7 +24,7 @@ export default function TicketForm({setTickets}) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (!formData.subject || !formData.description || !formData.email) {
+    if (!formData.subject || !formData.category || !formData.branch) {
       toast.error("Please fill in all required fields.");
       return;
     }
@@ -34,8 +33,8 @@ export default function TicketForm({setTickets}) {
       ticketId: Date.now(),
       subject: formData.subject,
       category: formData.category,
-      department: formData.department,
-      email: formData.email,
+      branch: formData.branch,
+      date: formData.date,
       priority: formData.priority,
       description: formData.description,
       resolved: false,
@@ -50,42 +49,111 @@ export default function TicketForm({setTickets}) {
     setFormData(initialFormState);
   };
 
-  const renderInputField = (key) => (
-    <div key={key}>
-      <label className="block font-medium capitalize text-gray-600 ">
-        {key} <span className="text-red-500">*</span>
-      </label>
-      {key === "priority" ? (
-        <select
-          name={key}
-          value={formData[key]}
-          onChange={handleChange}
-          className="w-full border p-2 rounded mt-1 text-gray-600 bg-white"
-        >
-          <option value="High">High</option>
-          <option value="Medium">Medium</option>
-          <option value="Low">Low</option>
-        </select>
-      ) : (
-        <input
-          type={key === "email" ? "email" : "text"}
-          name={key}
-          value={formData[key]}
-          onChange={handleChange}
-          className={`w-full border p-2 rounded mt-1 text-black bg-white ${
-            key === "description" ? "h-24" : ""
-          }`}
-        />
-      )}
-      {/* {errors[key] && <p className="text-red-500 text-sm">{errors[key]}</p>} */}
-    </div>
-  );
-
   return (
     <div className="w-full mx-auto bg-white p-6 rounded-lg shadow-lg">
       <h2 className="text-xl font-bold mb-4 text-gray-600">Create Ticket</h2>
       <form onSubmit={handleSubmit} className="space-y-4">
-        {Object.keys(formData).map(renderInputField)}
+        <div>
+          <label className="block font-medium capitalize text-gray-600">
+            Subject <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="text"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            className="w-full border p-2 rounded mt-1 text-black bg-white"
+          />
+        </div>
+
+        <div>
+          <label className="block font-medium capitalize text-gray-600">
+            Category <span className="text-red-500">*</span>
+          </label>
+          <select
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            className="w-full border p-2 rounded mt-1 text-gray-600 bg-white"
+          >
+            <option value="">Select Category</option>
+            <option value="Hardware Problem">Hardware Problem</option>
+            <option value="Software Problem">Software Problem</option>
+            <option value="Email Problem">Email Problem</option>
+          </select>
+        </div>
+
+        <div>
+          <label className="block font-medium capitalize text-gray-600">
+          Branch <span className="text-red-500">*</span>
+          </label>
+          {/* <select
+            name="department"
+            value={formData.department}
+            onChange={handleChange}
+            className="w-full border p-2 rounded mt-1 text-gray-600 bg-white"
+          >
+            <option value="">Select Department</option>
+            <option value="IT">IT</option>
+            <option value="Finance">Finance</option>
+            <option value="HR">HR</option>
+          </select> */}
+          <select
+            name="branch"
+            value={formData.branch}
+            onChange={handleChange}
+            className="w-full border p-2 rounded mt-1 text-gray-600 bg-white"
+          >
+            <option value="">Select Branch</option>
+            <option value="Fedha">Fedha</option>
+            <option value="Tassia">Tassia</option>
+            <option value="Utawala">Utawala</option>
+            <option value="Kitengela">Kitengela</option>
+            <option value="Machakos">Machakos</option>
+          </select>
+        </div>
+
+        <div>
+          <label className="block font-medium capitalize text-gray-600">
+            Date <span className="text-red-500">*</span>
+          </label>
+          <input
+            type="date"
+            name="date"
+            min={today}
+            onChange={handleChange}
+            className="w-full border p-2 rounded mt-1 text-gray-600 bg-white"
+          />
+        </div>
+
+        <div>
+          <label className="block font-medium capitalize text-gray-600">
+            Description <span className="text-red-500">*</span>
+          </label>
+          <textarea
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            className="w-full border p-2 rounded mt-1 text-black bg-white h-24"
+          />
+        </div>
+
+        <div>
+          <label className="block font-medium capitalize text-gray-600">
+            Priority <span className="text-red-500">*</span>
+          </label>
+          <select
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+            className="w-full border p-2 rounded mt-1 text-gray-600 bg-white"
+          >
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+          </select>
+        </div>
+
         <div className="flex justify-end space-x-2">
           <button
             type="button"
