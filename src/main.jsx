b38dfc734883299f35143316725fc9ebd3aeab4c
Janeirungu01 <jwirungu96@gmail.com>
2025-06-16 10:18:47 +0300

    include basename and use browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./Helpers/Api/AuthContext";
-
+import { BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Router>
-      <AuthProvider>
-      <Routes>
-        <Route path="/*" element={<App />}/>
-    </Routes>
-    </AuthProvider>
-    </Router>
+    <StrictMode>
+    <BrowserRouter basename="/desk">
+    <App />
+    </BrowserRouter>
   </StrictMode>,
 )
