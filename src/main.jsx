391d1b03ce1116060bb0cd855e90a72b424f1e4e
Janeirungu01 <jwirungu96@gmail.com>
2025-07-09 20:51:01 +0300

    remove basename

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-    <BrowserRouter basename="/helpdesk">
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </StrictMode>,
