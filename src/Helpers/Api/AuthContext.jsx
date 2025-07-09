7aa1b07f2a564a956a44f2a7283a680bd9f562c8
Janeirungu01 <jwirungu96@gmail.com>
2025-07-09 21:05:28 +0300

    store current user at localstorage

--- src/Helpers/Api/AuthContext.jsx
+++ src/Helpers/Api/AuthContext.jsx
@@ -1,30 +1,11 @@
-// import { createContext, useState, useContext } from "react";
-
-// const AuthContext = createContext();
-
-// export const AuthProvider = ({ children }) => {
-//   const [user, setUser] = useState(null);
-//   const [token, setToken] = useState(null);
-//   const [currentBranch, setCurrentBranch] = useState(null);
- 
-
-//   return (
-//     <AuthContext.Provider value={{ user, setUser, token, setToken, currentBranch, setCurrentBranch }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export const useAuth = () => useContext(AuthContext);
-
-
 import { createContext, useState, useContext } from "react";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  // const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
+  //   const [token, setToken] = useState(null);
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")));
   const [token, setToken] = useState(localStorage.getItem("token"))
   const [currentBranch, setCurrentBranch] = useState(null);
  
